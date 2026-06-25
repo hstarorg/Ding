dba8fa37bc7c6d67ea7a7350db77ea435f2ff009
hstarorg <hm910705@163.com>
2026-06-25 17:03:36 +0800

    chore: init project

---
+++ Ding/DingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DingApp.swift
+//  Ding
+//
+//  Created by Jay on 2026/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
